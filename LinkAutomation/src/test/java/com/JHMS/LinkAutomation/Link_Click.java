4a5094a4eeec27668df55dadc812c0a4b0c399c8
SuMukherjee <SuMukherjee@PRO-IND-390.ad.prolifics.com>
2019-10-17 16:25:56 +0530

    Added Project

---
+++ LinkAutomation/src/test/java/com/JHMS/LinkAutomation/Link_Click.java
@@ -0,0 +1,99 @@
+package com.JHMS.LinkAutomation;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Link_Click
+{
+	WebDriver driver;
+	 String spath = System.getProperty("user.dir");
+	   
+	
+	@BeforeMethod
+	public void launchurl()
+	{
+		 System.out.println("spath "+spath);
+		 System.setProperty("webdriver.chrome.driver", spath+"\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void Link() throws Exception
+	{
+		 File file=new File(spath+"\\Reources\\links.xlsx");
+		String fileName = "links.xlsx";
+		String sheetName = "sheet1";
+		//Create an object of FileInputStream class to read excel file
+
+		FileInputStream inputStream = new FileInputStream(file);
+
+		Workbook book = null;
+
+
+		//Find the file extension by splitting file name in substring  and getting only extension name
+
+		String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+		//Check condition if the file is xlsx file
+
+		if(fileExtensionName.equals(".xlsx")){
+
+			//If it is xlsx file then create object of XSSFWorkbook class
+
+			book = new XSSFWorkbook(inputStream);
+
+		}
+
+		//Check condition if the file is xls file
+
+		else if(fileExtensionName.equals(".xls")){
+
+			//If it is xls file then create object of HSSFWorkbook class
+
+			book = new HSSFWorkbook(inputStream);
+
+		}
+
+		//Read sheet inside the workbook by its name
+
+		Sheet sheet = book.getSheet(sheetName);
+
+		//Find number of rows in excel file
+
+		int rowCount = sheet.getLastRowNum()-sheet.getFirstRowNum();
+        for (int i = 0; i < rowCount+1; i++) {
+		Row row = sheet.getRow(i);
+			{
+				org.apache.poi.ss.usermodel.Cell cell=row.getCell(0);
+				String cellValue=cell.getStringCellValue();
+                System.out.println(cellValue);
+                driver.get(cellValue);
+				//driver.navigate().to(cellValue);
+				driver.manage().window().maximize();
+				Thread.sleep(2000);
+			}
+
+		}
+	}
+	
+	
+	@AfterMethod
+	public void closedriver()
+	{
+		driver.close();
+	}
+	
+}
